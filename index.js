23e5cdfccd07dafc1614b78b4eeebb26c1b5b206
Seth Wooten <slwooten14@gmail.com>
2022-03-22 22:38:27 -0400

    required employee, manager, engineer, and intern into index.js from their respective files

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const team = [];
 
 const chooseEmployee = (type, neededInfo) => {
     return inquirer.prompt([
